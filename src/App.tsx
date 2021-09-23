b6ce6ece47053881344003d9e9bcc6377a351dfb
mkessy <kokokessy@gmail.com>
2021-09-22 23:40:50 -0700

    hacking away at integrating d3.drag with useRef canvas

--- src/App.tsx
+++ src/App.tsx
@@ -7,54 +7,105 @@ import { PlayerOnBoard } from "./types/Player";
 import { fetchPlayerData } from "./utils/fetchPlayerData";
 import {
   schemeCategory10,
+  select,
   drag,
   D3DragEvent,
   DraggedElementBaseType,
 } from "d3";
-import dragsubject from "./utils/canvas/drag";
 
 const radius = 16;
+const width = 800;
+const height = 600;
 
-const draw = (ctx: CanvasRenderingContext2D, player: PlayerOnBoard) => {
-  ctx.moveTo(player.x + radius, player.y);
-  ctx.arc(player.x, player.y, radius, 0, 2 * Math.PI);
-  ctx.fillStyle = player.color;
+const initialPlayers = require("./testPlayers.json");
+
+const draw = (ctx: CanvasRenderingContext2D | null, player: PlayerOnBoard) => {
+  if (ctx) {
+    ctx.moveTo(player.x + radius, player.y);
+    ctx.arc(player.x, player.y, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = player.color;
+  }
 };
 
 function App() {
-  const [players, setPlayers] = useState<PlayerOnBoard[]>([]);
+  const [players, setPlayers] = useState<PlayerOnBoard[]>(initialPlayers);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const dragsubject = (
+    event: D3DragEvent<DraggedElementBaseType, null, PlayerOnBoard>
+  ) => {
+    //console.log(event);
+    let subject: PlayerOnBoard | null = null;
+    let distance = Infinity;
+    for (const c of players) {
+      let d = Math.hypot(event.x - c.x, event.y - c.y);
+      if (d < distance) {
+        distance = d;
+        subject = c;
+      }
+    }
+    return subject;
+  };
 
   const dragstarted = (
     event: D3DragEvent<DraggedElementBaseType, null, PlayerOnBoard>
   ) => {
+    //console.log(event.x, event.y);
     const playersCopy: PlayerOnBoard[] = [...players];
     setPlayers([...playersCopy].splice(players.indexOf(event.subject), 1));
     setPlayers([...players, event.subject]);
   };
 
-  const drag = drag()
+  //need to be clever about how we change the state here...
+  //we should probably store the players by id. then i can easily
+  const dragged = (
+    event: D3DragEvent<DraggedElementBaseType, null, PlayerOnBoard>
+  ) => {
+    //console.log(event.x + " " + event.y);
+    //console.log("Dragging: " + event.subject.id);
+  };
+
+  const dragended = (
+    event: D3DragEvent<DraggedElementBaseType, null, PlayerOnBoard>
+  ) => {
+    //console.log("finished dragging: " + event.subject.id);
+  };
+
+  const canvasDrag = drag<HTMLCanvasElement, unknown, unknown>()
     .subject(dragsubject)
     .on("start", dragstarted)
     .on("drag", dragged)
     .on("end", dragended);
 
-  React.useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext("2d");
-    ctx?.clearRect(0, 0, window.innerHeight, window.innerWidth);
+  const canvas = canvasRef.current;
+  if (canvas) {
+    const canvasSelection = select(canvas);
+    canvasSelection.call(canvasDrag);
+  }
+
+  useEffect(() => {
+    const ctx = canvas ? canvas.getContext("2d") : null;
+    ctx?.clearRect(0, 0, width, height);
     players.forEach((player) => draw(ctx, player));
   });
+
+  console.log("Rendering canvas");
+  console.log(canvasDrag);
 
   return (
     <div className="border-solid rounded border-r-4 border-gray-300">
       <canvas
         ref={canvasRef}
-        width={window.innerWidth}
-        height={window.innerHeight}
-        onClick={(e) => {
-          const newPlayer = { x: e.clientX, y: e.clientY };
-          setPlayers([...players, newPlayer]);
+        width={width}
+        height={height}
+        onClick={() => {
+          const ctx = canvasRef.current
+            ? canvasRef.current.getContext("2d")
+            : null;
+          //console.log(players);
+          for (const p of players) {
+            draw(ctx, p);
+          }
         }}
       />
     </div>
