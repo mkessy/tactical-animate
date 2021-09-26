e70284dff38d09b6ce25ac35cd66b7dd6043fb09
mkessy <kokokessy@gmail.com>
2021-09-25 18:39:39 -0700

    implemented d3-drag on canvas inside component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React, { DragEvent, MouseEvent, SyntheticEvent, useRef } from "react";
+import React, {
+  DragEvent,
+  MouseEvent,
+  SyntheticEvent,
+  useCallback,
+  useRef,
+} from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { PureCanvas, Canvas } from "./components/TacticalBoard";
+import { CanvasContainer } from "./components/TacticalBoard";
 import { useEffect, useState } from "react";
 import { PlayerOnBoard } from "./types/Player";
 import { fetchPlayerData } from "./utils/fetchPlayerData";
@@ -13,25 +19,17 @@ import {
   D3DragEvent,
   DraggedElementBaseType,
 } from "d3";
-import { isContext } from "vm";
 
 const radius = 16;
 const width = 500;
 const height = 500;
 
-//const initialPlayers = require("./testPlayers.json");
-
-const draw = (ctx: CanvasRenderingContext2D | null, player: PlayerOnBoard) => {
-  if (ctx) {
-    ctx.beginPath();
-    ctx.moveTo(player.x + radius, player.y);
-    ctx.arc(player.x, player.y, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = player.color;
-    ctx.fill();
-    ctx.lineWidth = 1;
-    ctx.stroke();
-  }
+type CanvasContainerProps = {
+  playerData: PlayerOnBoard[];
+  setPlayerData: (players: PlayerOnBoard[]) => void;
 };
+
+//const initialPlayers = require("./testPlayers.json");
 
 const initialPlayers: PlayerOnBoard[] = range(20).map((i) => ({
   id: i,
@@ -42,55 +40,30 @@ const initialPlayers: PlayerOnBoard[] = range(20).map((i) => ({
 
 function App() {
   const [players, setPlayers] = useState<PlayerOnBoard[]>(initialPlayers);
-  const canvasContainerRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    const canvasContainer = select(canvasContainerRef.current);
-
-    const dragsubject = (
-      event: D3DragEvent<HTMLCanvasElement, unknown, unknown>
-    ) => {
-      let subject = null;
-      let distance = Infinity;
-      for (const p of players) {
-        let d = Math.hypot(event.x - p.x, event.y - p.y);
-        if (d < distance) {
-          distance = d;
-          subject = p;
-        }
-      }
-      return subject;
-    };
-
-    const canvasDrag = drag()
-      .subject(dragsubject)
-      .on("start", (e) => {
-        console.error(
-          "drag started near: " + e.subject.id + " " + e.subject.color
-        );
-      })
-      .on("drag", (e) => console.error("dragged"))
-      .on("end", (e) => {
-        console.error("drag ended near" + e.subject.id + " " + e.subject.color);
-      });
-
-    canvasContainer.call(canvasDrag as any);
-
-    const ctx = canvasContainer.node()?.getContext("2d");
-
-    players.forEach((p, i) => {
-      if (ctx) {
-        draw(ctx, p);
-      }
-    });
-  }, [players]);
-
-  console.log("Rendering canvas");
+  const onDragPlayer = (players: PlayerOnBoard[]) => {
+    console.error(players);
+    setPlayers(players);
+  };
 
   return (
-    <div className="max-w-screen-lg mx-auto">
+    <div className="flex max-w-screen-lg mx-auto">
       <div className="m-3 border border-solid border-gray-600">
-        <PureCanvas width={width} height={height} ref={canvasContainerRef} />
+        <CanvasContainer
+          setPlayerData={onDragPlayer}
+          playerData={initialPlayers}
+        />
+      </div>
+      <div>
+        <ul>
+          {players.map((p, i) => {
+            return (
+              <li key={p.id}>
+                {p.id} --- {p.x},{p.y}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
--- src/components/TacticalBoard.tsx
+++ src/components/TacticalBoard.tsx
@@ -1,11 +1,24 @@
 import { PlayerOnBoard } from "../types/Player";
 import BoardDimensions from "../types/TacticalBoard";
 import useTacticalBoardCanvas from "../hooks/useTacticalBoardCanvas";
-import { forwardRef, useEffect, useRef, useState } from "react";
+import {
+  forwardRef,
+  FunctionComponent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { select, D3DragEvent, drag } from "d3";
+import { JsxElement } from "typescript";
 
 type CanvasProps = {
   width: number;
   height: number;
+};
+
+type CanvasContainerProps = {
+  playerData: PlayerOnBoard[];
+  setPlayerData: (players: PlayerOnBoard[]) => void;
 };
 
 export const PureCanvas = forwardRef<HTMLCanvasElement, CanvasProps>(
@@ -21,6 +34,62 @@ export const Canvas = (props: CanvasProps) => {
   );
 };
 
-export const CanvasContainer = (playerData: PlayerOnBoard[]) => {
-  const [players, setPlayers] = useState<PlayerOnBoard[]>(playerData);
+export const CanvasContainer = (props: CanvasContainerProps) => {
+  const radius = 16;
+  const width = 500;
+  const height = 500;
+
+  const canvasContainerRef = useRef<HTMLCanvasElement>(null);
+  const playersToDrag = useRef<PlayerOnBoard[]>(props.playerData);
+
+  useEffect(() => {
+    const canvasContainer = select(canvasContainerRef.current);
+
+    const dragsubject = (
+      event: D3DragEvent<HTMLCanvasElement, unknown, unknown>
+    ) => {
+      let subject = null;
+      let distance = Infinity;
+      for (const p of playersToDrag.current) {
+        let d = Math.hypot(event.x - p.x, event.y - p.y);
+        if (d < distance) {
+          distance = d;
+          subject = p;
+        }
+      }
+      return subject;
+    };
+
+    const ctx = canvasContainer.node()?.getContext("2d");
+    const canvasDrag = drag()
+      .subject(dragsubject)
+      .on("start", (e) => {
+        const players = playersToDrag.current;
+        players.splice(players.indexOf(e.subject), 1);
+        players.push(e.subject);
+        playersToDrag.current = players;
+      })
+      .on("drag", (e) => {
+        e.subject.x = Math.max(0, Math.min(width, e.x));
+        e.subject.y = Math.max(0, Math.min(height, e.y));
+      })
+      .on("end", (e) => {})
+      .on("start.draw drag.draw end.draw", () => {
+        ctx?.clearRect(0, 0, width, height);
+        for (const { x, y, color } of playersToDrag.current) {
+          if (ctx) {
+            ctx.beginPath();
+            ctx.moveTo(x + radius, y);
+            ctx.arc(x, y, radius, 0, 2 * Math.PI);
+            ctx.fillStyle = color;
+            ctx.fill();
+          }
+        }
+        props.setPlayerData(playersToDrag.current);
+      });
+
+    canvasContainer.call(canvasDrag as any);
+  }, [props]);
+
+  return <PureCanvas width={width} height={height} ref={canvasContainerRef} />;
 };
